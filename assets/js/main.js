cad04cab1bfc47312230dd401797a936c4e1ff7c
md.samiul haque Siddique <samiulsabbir8262@gmail.com>
2021-07-08 16:06:24 +0600

    upload first time in orion instalogic

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function(){ 
+  AOS.init();
+  $('.carousel').carousel({
+    interval: 4000,
+  });
+
+  window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+  // slider product 
+  $(".owl-carousel").owlCarousel({
+loop:true,
+autoplay:true,
+autoplayHoverPause:true,
+autoplayTimeout:4000,
+responsive:{
+      0:{
+          items:1, 
+      },
+      600:{
+          items:2,
+      },
+      1000:{
+          items:4,
+      }
+    }
+  });
+
+});
+
+ 
+
+
+

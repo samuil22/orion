9070a66ab64df5f62ee05a7bd51490742a87c3eb
md.samiul haque Siddique <samiulsabbir8262@gmail.com>
2021-07-09 15:40:47 +0600

    mobile fix issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){ 
-  AOS.init();
+ 
   $('.carousel').carousel({
     interval: 4000,
   });
@@ -15,7 +15,7 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= sticky){
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
